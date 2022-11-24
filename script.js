79ce5dcedcb15f0920abc43a212ec765848c9c6a
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-11-24 13:50:29 -0300

    code

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Efeito máquina de escrever
+const tituloAreaInicial = document.querySelector("h2");
+const titulo = document.querySelector("h3");
+const span = document.querySelector("span");
+const a = document.querySelector("p");
+
+function efeitoEscrever(el) {
+    const textoNoArray = el.innerHTML.split("");
+    el.innerHTML = '';
+    if (el == a) {
+        setTimeout(() => {
+            textoNoArray.forEach((letra, index) => {
+                setTimeout(() => el.innerHTML += letra, 15 * index);
+            });
+        }, 3000);
+    }
+    else if (el == span) {
+        setTimeout(() => {
+            textoNoArray.forEach((letra, index) => {
+                setTimeout(() => el.innerHTML += letra, 40 * index);
+            });
+        }, 1500);
+    }
+    else {
+        textoNoArray.forEach((letra, index) => {
+            setTimeout(() => el.innerHTML += letra, 60 * index);
+        });
+    }
+
+}
+
+efeitoEscrever(titulo);
+efeitoEscrever(span);
+efeitoEscrever(a);
